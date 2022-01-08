ea59dd06549d440551be0ab18e1123bb0d29c4dd
Greger Sundvall <gsundvall@gmail.com>
2022-01-08 13:04:57 +0100

    Cartesian grid done

--- Assets/Scripts/Editor/Snapper.cs
+++ Assets/Scripts/Editor/Snapper.cs
@@ -16,7 +16,6 @@ public static class Snapper
             // Save what you are changing into an undo string!
             Undo.RecordObject(go.transform, UNDO_STR_SNAP);
             go.transform.position = go.transform.position.Round();
-
         }
     }
     // Optional validation function. Make sure string is the same.
@@ -27,11 +26,5 @@ public static class Snapper
     }
     
     
-    public static Vector3 Round(this Vector3 v)
-    {
-        v.x = Mathf.Round(v.x);
-        v.y = Mathf.Round(v.y);
-        v.z = Mathf.Round(v.z);
-        return v;
-    }
+
 }
--- Assets/Scripts/Editor/SnapperTool.cs
+++ Assets/Scripts/Editor/SnapperTool.cs
@@ -4,19 +4,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class SnapperTool : EditorWindow
 {
 
+    public float gridSize = 1f;
+    private SerializedObject so;
+    private SerializedProperty propGridSize;
+
     [MenuItem("Tools/Snapper Tool")]
-    public static void OpenTheThing()
-    {
-        GetWindow<SnapperTool>("Snapper");
-    }
+    public static void OpenTheThing() => GetWindow<SnapperTool>("Snapper");
 
 
     private void OnEnable()
     {
+        so = new SerializedObject(this);
+        propGridSize = so.FindProperty("gridSize");
+        
         Selection.selectionChanged += Repaint;
         SceneView.duringSceneGui += DuringSceneGUI; // SnapperTool is now drawing a line in scene.
     }
@@ -30,11 +35,43 @@ public class SnapperTool : EditorWindow
     // duringSceneGUI is the editors update which runs at like 100 fps
     void DuringSceneGUI(SceneView sceneView)
     {
-        Handles.DrawLine(Vector3.zero, Vector3.up);
+        if (Event.current.type == EventType.Repaint) // Prevents running constantly.
+        {
+            Handles.zTest = CompareFunction.LessEqual;
+        
+            const float gridDrawExtent = 16;
+            int lineCount = Mathf.RoundToInt((gridDrawExtent * 2) / gridSize);
+            if (lineCount % 2 == 0)
+            {
+                lineCount++;
+            }
+            int halfLineCount = lineCount / 2;
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                int intOffset = i - halfLineCount;
+                float xCoord = intOffset * gridSize;
+                float zCoord0 = halfLineCount * gridSize;
+                float zCoord1 = -halfLineCount * gridSize;
+            
+                Vector3 p0 = new Vector3(xCoord, 0f, zCoord0);
+                Vector3 p1 = new Vector3(xCoord, 0f, zCoord1);
+                Handles.DrawAAPolyLine(p0, p1);
+            
+                p0 = new Vector3(zCoord0, 0f, xCoord);
+                p1 = new Vector3(zCoord1, 0f, xCoord);
+                Handles.DrawAAPolyLine(p0, p1);
+            }
+        }
+
     }
 
     void OnGUI()
     {
+        so.Update();
+        EditorGUILayout.PropertyField(propGridSize);
+        so.ApplyModifiedProperties();
+        
         using (new EditorGUI.DisabledScope(Selection.gameObjects.Length == 0))
         {
             if (GUILayout.Button("Snap Selection"))
@@ -42,16 +79,15 @@ public class SnapperTool : EditorWindow
                 SnapSelection();
             }
         }
-        
     }
-    
+
     void SnapSelection()
     {
         foreach (var go in Selection.gameObjects)
         {
             // Save what you are changing into an undo string!
             Undo.RecordObject(go.transform, "snap objects");
-            go.transform.position = go.transform.position.Round();
+            go.transform.position = go.transform.position.Round(gridSize);
 
         }
     }
